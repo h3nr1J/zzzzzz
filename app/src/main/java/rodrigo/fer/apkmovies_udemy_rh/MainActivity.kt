73bcba999c433b26e0088be3285d74d8cd3ed89a
h3nr1j <d.henry.7569@gmail.com>
2022-08-07 18:30:19 -0500

    Creacion de la interfaz main

--- app/src/main/java/rodrigo/fer/apkmovies_udemy_rh/MainActivity.kt
+++ app/src/main/java/rodrigo/fer/apkmovies_udemy_rh/MainActivity.kt
@@ -2,10 +2,17 @@ package rodrigo.fer.apkmovies_udemy_rh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Para eliminar los iconos del wifi etc
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
---
+++ app/src/main/java/rodrigo/fer/apkmovies_udemy_rh/ui/MainFragment.kt
@@ -0,0 +1,18 @@
+package rodrigo.fer.apkmovies_udemy_rh.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import rodrigo.fer.apkmovies_udemy_rh.R
+
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
